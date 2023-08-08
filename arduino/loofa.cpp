9153ffdc0d72bbd0e78137bf3160819083cbfa73
Mateusz <101819371+dmouze@users.noreply.github.com>
2023-08-08 14:17:11 +0200

    testy połączenia z aplikacją android

--- arduino/loofa.cpp
+++ arduino/loofa.cpp
@@ -1,68 +1,22 @@
-#include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
+#include "BluetoothSerial.h"
 
-const int ledPin = 2; 
-bool ledState = false;
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
-bool deviceConnected = false;
-
-class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-    deviceConnected = true;
-  }
-
-  void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
-  }
-};
+BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
-
-  BLEDevice::init("Loofa na lezaco");
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  BLEService *pService = pServer->createService(BLEUUID((uint16_t)0x180D));
-
-  pCharacteristic = pService->createCharacteristic(
-                      BLEUUID((uint16_t)0x2A37),
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE
-                    );
-
-  pCharacteristic->addDescriptor(new BLE2902());
-
-  pService->start();
-
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->addServiceUUID(BLEUUID((uint16_t)0x180D));
-  pAdvertising->start();
+  SerialBT.begin("Lufa"); //Bluetooth device name
 }
 
 void loop() {
-  if (deviceConnected) {
-    std::string value = pCharacteristic->getValue();
-    if (value.length() > 0) {
-      // Odczyt wartości przysłanej z aplikacji Android
-      char command = value[0];
-      if (command == '1') {
-        // Wysłano sygnał '1' - zapal diodę LED
-        digitalWrite(ledPin, HIGH);
-        ledState = true;
-        Serial.println("Dioda zapalona!");
-      } else if (command == '0') {
-        // Wysłano sygnał '0' - zgaś diodę LED
-        digitalWrite(ledPin, LOW);
-        ledState = false;
-        Serial.println("Dioda zgaszona!");
-      }
-    }
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
   }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
 }
