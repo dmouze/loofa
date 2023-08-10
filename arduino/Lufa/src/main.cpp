03841d50b1de4c3033be31596083b3867dfff0fd
deadmouze <mateusz.kierman@gmail.com>
2023-08-10 08:51:59 +0200

    dmze
    naprawione odczytywanie komend przez arduino

--- arduino/Lufa/src/main.cpp
+++ arduino/Lufa/src/main.cpp
@@ -1,76 +1,42 @@
 #include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#error Bluetooth is not enabled! Please run `make menuconfig` to enable it
 #endif
 
 BluetoothSerial SerialBT;
-int received;// received value will be stored in this variable
-char receivedChar;// received value will be stored as CHAR in this variable
-const int ledPin = 2 ; // Pin 2 jako wyjściowy, podłączony do diody LED
+int received;         // odebrana zmienna
+char receivedChar;    // zmienna przechowywana jako char
+const int ledPin = 2; // wyjscie drugie jako LED
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  SerialBT.begin("LufaNaLezaco"); //Bluetooth device name
+  SerialBT.begin("LufaNaLezaco"); // nazwa urządzenia Bluetooth
+  pinMode(ledPin, OUTPUT);        // pin LED jako wyjscie
 }
 
-void loop() {
-  if (Serial.available()) {
+void loop()
+{
+  if (Serial.available())
+  {
     SerialBT.write(Serial.read());
   }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  if (Serial.available()) {
-SerialBT.write(Serial.read());
-}
- if (SerialBT.available()) {
-    if (receivedChar == 'a'){
-      digitalWrite(ledPin, HIGH); // Włączenie diody (ustawienie stanu wysokiego)
+
+  if (SerialBT.available())
+  {
+    receivedChar = SerialBT.read();
+
+    if (receivedChar == 'a')
+    {
+      digitalWrite(ledPin, HIGH);
     }
 
-    if (receivedChar == 'b') {
-      digitalWrite(ledPin, LOW); // Wyłączenie diody (ustawienie stanu niskiego)
+    if (receivedChar == 'b')
+    {
+      digitalWrite(ledPin, LOW);
     }
   }
+
   delay(20);
 }
-
-
-
-
-// #include "BluetoothSerial.h"
-
-// #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-// #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-// #endif
-
-// BluetoothSerial SerialBT;
-// int received;// received value will be stored in this variable
-// char receivedChar;// received value will be stored as CHAR in this variable
-// const int ledPin = 2 ; // Pin 2 jako wyjściowy, podłączony do diody LED
-
-// void setup() {
-//   Serial.begin(115200);
-//   SerialBT.begin("LufaNaLezaco"); //Bluetooth device name
-//   pinMode(ledPin, OUTPUT);
-// }
-
-// void loop() {
-//   receivedChar =(char)SerialBT.read();
-
-//   if (Serial.available()) {
-//     SerialBT.write(Serial.read());
-//   }
-//   if (SerialBT.available()) {
-//     if (receivedChar == 'a'){
-//       digitalWrite(ledPin, HIGH); // Włączenie diody (ustawienie stanu wysokiego)
-//     }
-
-//     if (receivedChar == 'b') {
-//       digitalWrite(ledPin, LOW); // Wyłączenie diody (ustawienie stanu niskiego)
-//     }
-//   }
-//   delay(20);
-// }
